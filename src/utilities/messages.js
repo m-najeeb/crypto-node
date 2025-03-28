a7c844973ddcabbf0537f8d20a8e8131abf1ca0f
m-najeeb <muhammadnajeeb2022@gmail.com>
2025-03-28 15:14:58 +0500

    expenses speeling correction

--- src/utilities/messages.js
+++ src/utilities/messages.js
@@ -4,7 +4,7 @@ module.exports = {
   INVALID_DATA: "Invalid Data",
   NO_DATA_FOUND: "No Data Found",
   NO_EXPENSES_IN_THIS_CATEGORY: "No Expenses In This Category",
-  NO_EXPENSES_USING_THIS_PAYMENT_METHOD: "No Expeses Using This Paymet Method",
+  NO_EXPENSES_USING_THIS_PAYMENT_METHOD: "No Expenses Using This Paymet Method",
   SUCCESSFULLY_ADDED: "Successfully Added",
   SUCCESSFULLY_SENT: "Successfully Sent",
 };
